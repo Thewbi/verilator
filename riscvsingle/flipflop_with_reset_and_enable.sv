1253b094465dab88818c7d1f0df1b61251dd61fe
thewbi <thewbi@example.com>
2024-08-25 19:13:38 +0200

    Added riscvsingle implementation from Harris & Harris, Digital Design and Computer Architecture RISC-V edition

---
+++ riscvsingle/flipflop_with_reset_and_enable.sv
@@ -0,0 +1,12 @@
+module flopenr(
+	input logic clk, reset, en,
+	input logic [7:0] d,
+	output logic [7:0] q);
+ 
+	always_ff @(posedge clk, posedge reset)
+		if (reset)	
+			q <= 0;
+		else if (en) 
+			q <= d;
+ 
+endmodule
